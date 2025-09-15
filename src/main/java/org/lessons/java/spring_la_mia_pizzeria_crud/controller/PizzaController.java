f473b8bc3eaf6fa69c26d1eafba8d1ba9ad9b7c1
vmqxdv <vquintilio@gmail.com>
2025-09-15 12:32:22 +0200

    feat: Step 2

--- src/main/java/org/lessons/java/spring_la_mia_pizzeria_crud/controller/PizzaController.java
+++ src/main/java/org/lessons/java/spring_la_mia_pizzeria_crud/controller/PizzaController.java
@@ -1,6 +1,7 @@
 package org.lessons.java.spring_la_mia_pizzeria_crud.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.lessons.java.spring_la_mia_pizzeria_crud.model.Pizza;
 import org.lessons.java.spring_la_mia_pizzeria_crud.repository.PizzaRepository;
@@ -8,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -24,5 +26,14 @@ public class PizzaController {
     model.addAttribute("pizzas", pizzas);
 
     return "pizzas/index";
+  }
+
+  @GetMapping("/{id}")
+  public String show(@PathVariable("id") Integer id, Model model) {
+    Optional<Pizza> pizza = repository.findById(id);
+
+    model.addAttribute("pizza", pizza.orElse(null));
+
+    return "pizzas/show";
   }
 }
