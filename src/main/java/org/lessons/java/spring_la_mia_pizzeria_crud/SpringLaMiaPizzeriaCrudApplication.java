5be2d9037d6a4590588b0343b7b8d369e5ddc3f7
vmqxdv <vquintilio@gmail.com>
2025-09-15 11:04:03 +0200

    first commit

---
+++ src/main/java/org/lessons/java/spring_la_mia_pizzeria_crud/SpringLaMiaPizzeriaCrudApplication.java
@@ -0,0 +1,13 @@
+package org.lessons.java.spring_la_mia_pizzeria_crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLaMiaPizzeriaCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringLaMiaPizzeriaCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/lessons/java/spring_la_mia_pizzeria_crud/SpringLaMiaPizzeriaCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package org.lessons.java.spring_la_mia_pizzeria_crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLaMiaPizzeriaCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
