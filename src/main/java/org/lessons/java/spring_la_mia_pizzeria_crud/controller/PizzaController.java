c37bb9db1191b5380deb7a4d892049c8f189487e
vmqxdv <vquintilio@gmail.com>
2025-09-16 22:17:37 +0200

    feat: Added Delete Crud

--- src/main/java/org/lessons/java/spring_la_mia_pizzeria_crud/controller/PizzaController.java
+++ src/main/java/org/lessons/java/spring_la_mia_pizzeria_crud/controller/PizzaController.java
@@ -77,4 +77,11 @@ public class PizzaController {
 
     return "redirect:/pizzas";
   }
+
+  @PostMapping("/delete/{id}")
+  public String delete(@PathVariable Integer id) {
+    repository.deleteById(id);
+
+    return "redirect:/pizzas";
+  }
 }
