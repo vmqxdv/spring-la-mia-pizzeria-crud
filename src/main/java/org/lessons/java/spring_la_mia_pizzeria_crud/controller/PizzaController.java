760199ee0b80dc18aa9d7aadbfbffdfdc36b2b05
vmqxdv <vquintilio@gmail.com>
2025-09-16 21:52:38 +0200

    feat: Added Update crud

--- src/main/java/org/lessons/java/spring_la_mia_pizzeria_crud/controller/PizzaController.java
+++ src/main/java/org/lessons/java/spring_la_mia_pizzeria_crud/controller/PizzaController.java
@@ -60,4 +60,21 @@ public class PizzaController {
 
     return "redirect:/pizzas";
   }
+
+  @GetMapping("/edit/{id}")
+  public String edit(@PathVariable Integer id, Model model) {
+    model.addAttribute("pizza", repository.findById(id).get());
+
+    return "pizzas/edit";
+  }
+
+  @PostMapping("/edit/{id}")
+  public String update(@Valid @ModelAttribute("pizza") Pizza formPizza, BindingResult bindingResult, Model model) {
+    if (bindingResult.hasErrors())
+      return "pizzas/edit";
+
+    repository.save(formPizza);
+
+    return "redirect:/pizzas";
+  }
 }
